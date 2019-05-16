9bfedb6202fcf0ca7034432d8384156c9199a526
OBarois <olivier.barois@esa.int>
2019-05-16 17:08:34 +0200

    Updates

---
+++ precache-manifest.99685c2c0972939359fb829a11c6b488.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c13103e7c4ff98a3abd3",
+    "url": "/reactwww/static/js/runtime~main.b5abcd5d.js"
+  },
+  {
+    "revision": "56ddd8b2125f7ab8a166",
+    "url": "/reactwww/static/js/main.8e7bc319.chunk.js"
+  },
+  {
+    "revision": "e00262e90dae775f5aa1",
+    "url": "/reactwww/static/js/2.51424d52.chunk.js"
+  },
+  {
+    "revision": "56ddd8b2125f7ab8a166",
+    "url": "/reactwww/static/css/main.097d1a79.chunk.css"
+  },
+  {
+    "revision": "0d37e8d0cc99562292ad509ff545a6b6",
+    "url": "/reactwww/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactwww/precache-manifest.c660e52fa1c335aa23f6234517a5de42.js"
+  "/reactwww/precache-manifest.99685c2c0972939359fb829a11c6b488.js"
 );
 
 workbox.clientsClaim();
