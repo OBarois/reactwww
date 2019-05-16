5b524597df8b462a0b344bb1b07c347283364aa8
OBarois <olivier.barois@esa.int>
2019-05-16 17:07:48 +0200

    added mission selector

--- src/missionselector.js
+++ src/missionselector.js
@@ -1,25 +1,33 @@
 import React, { useEffect } from "react";
 import { useGlobal } from 'reactn';
 import { useHotkeys } from 'react-hotkeys-hook';
+import './missionselector.css'
 
 
 
 
 function MissionSelector() {
 
-console.log('mission selector')
+
     const [ mission, setMission ] = useGlobal('mission');
-    console.log("selected: "+mission)
     useHotkeys("1",()=>{setMission('S1')}) 
     useHotkeys("2",()=>{setMission('S2')}) 
     useHotkeys("3",()=>{setMission('S3')}) 
     useHotkeys("5",()=>{setMission('S5P')}) 
     useHotkeys("6",()=>{setMission('ENVISAT')}) 
 
-
+    useEffect(() => {
+        console.log('Mission changed to: '+ mission)
+    }, [mission]);
 
     return (
-        <div></div>
+        <div className='MissionSelector'>
+            <img className='MissionIcon' src='./images/s1_black.png' onClick={() => setMission('S1')} />
+            <img className='MissionIcon' src='./images/s2_black.png' onClick={() => setMission('S2')} />
+            <img className='MissionIcon' src='./images/s3_black.png' onClick={() => setMission('S3')} />
+            <img className='MissionIcon' src='./images/s5p_black.png' onClick={() => setMission('S5P')} />
+            <img className='MissionIcon' src='./images/envisat.png' onClick={() => setMission('ENVISAT')} />
+        </div>
     )
 }
 
