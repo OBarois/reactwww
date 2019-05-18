2e0e44866dde7a90cdba97e7450a83d88d428d29
Oli <oli@MacOli.local>
2019-05-18 18:54:50 +0200

    Updates

---
+++ precache-manifest.8c87d3c168dac621472f21aa65be373f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5cb2999e296d3048648",
+    "url": "/eoi/static/js/runtime~main.6509997a.js"
+  },
+  {
+    "revision": "aa78d46a8e6f229d0f32",
+    "url": "/eoi/static/js/main.550aba90.chunk.js"
+  },
+  {
+    "revision": "aba4187b12d03d982e17",
+    "url": "/eoi/static/js/2.3d7e863b.chunk.js"
+  },
+  {
+    "revision": "aa78d46a8e6f229d0f32",
+    "url": "/eoi/static/css/main.dd4e0fe3.chunk.css"
+  },
+  {
+    "revision": "18a511c3f80dbce922e580a2b9753155",
+    "url": "/eoi/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactwww/precache-manifest.7ca6df1ff0a86b0c6d90126e495690aa.js"
+  "/eoi/precache-manifest.8c87d3c168dac621472f21aa65be373f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactwww/index.html", {
+workbox.routing.registerNavigationRoute("/eoi/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
