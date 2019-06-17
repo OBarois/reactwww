388f9d1489e8905fd95108f8f10b7f35dbf44307
OBarois <olivier.barois@esa.int>
2019-06-17 12:07:24 +0200

    Updates

---
+++ precache-manifest.29e1e5d562658217b8c65cd15434fa20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f791da9553b22a718f388dec659fc7a1",
+    "url": "/reactwww/index.html"
+  },
+  {
+    "revision": "d3414f85d15810938e81",
+    "url": "/reactwww/static/css/main.42d3a0ff.chunk.css"
+  },
+  {
+    "revision": "c0278bad9090e7b16850",
+    "url": "/reactwww/static/js/2.3f59d517.chunk.js"
+  },
+  {
+    "revision": "d3414f85d15810938e81",
+    "url": "/reactwww/static/js/main.64af1710.chunk.js"
+  },
+  {
+    "revision": "c13103e7c4ff98a3abd3",
+    "url": "/reactwww/static/js/runtime~main.b5abcd5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactwww/precache-manifest.65391a2687f16b3ea02efb4184aac11b.js"
+  "/reactwww/precache-manifest.29e1e5d562658217b8c65cd15434fa20.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactwww/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactwww/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
